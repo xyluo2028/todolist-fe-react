7e407b1c3dd3546b58d617ab855284622b0f093d
LuoXinyu | Ruff <xinyu.luo@rakuten.com>
2025-10-07 18:49:13 +0900

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:7071',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:7071',
 });
 
 const authConfig = (auth) => ({
